35a269b31602e5cd6e8649fca885c12c82000863
Travis Johnston <tjohnst1@gmail.com>
2016-01-10 15:04:21 -0800

    setup grunt config

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt){
+
+  grunt.initConfig({
+      compass: {
+        dist: {
+          options: {
+            sassDir: './app/src/scss',
+            cssDir: './app/dist'
+          }
+        }
+      },
+      watch: {
+        files: "./app/src/scss/*.scss",
+        tasks: ['compass']
+      }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['watch'])
+
+}
