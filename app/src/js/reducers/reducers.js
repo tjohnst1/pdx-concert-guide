31f0eceef2040f632e657ce718e5d4346927a0dc
Travis Johnston <tjohnst1@gmail.com>
2016-01-18 14:31:40 -0800

    create simple reducer

---
+++ app/src/js/reducers/reducers.js
@@ -0,0 +1,21 @@
+import { combineReducers } from 'react-redux'
+import { REQUEST_LISTINGS, RECEIVE_LISTINGS } from '../constants/constants'
+
+let initialState = {listings: [], isFetching: false}
+
+function posts(state = initialState, action){
+  switch (action.type){
+    case REQUEST_LISTINGS:
+      return Object.assign({}, state, {isFetching: true})
+    case RECEIVE_LISTINGS:
+      return Object.assign({}, state, {isFetching: false, listings: action.listings})
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({
+  posts
+})
+
+export default rootReducer
