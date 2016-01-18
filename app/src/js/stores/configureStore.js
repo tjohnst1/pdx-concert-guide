165167af968e712faf928724baee14acf3fdac0c
Travis Johnston <tjohnst1@gmail.com>
2016-01-18 15:32:03 -0800

    create store configuration; refactor entrypoint file

---
+++ app/src/js/stores/configureStore.js
@@ -0,0 +1,11 @@
+import {createStore, applyMiddleware} from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import createLogger from 'redux-logger'
+import rootReducer from '../reducers/reducers'
+
+const createStoreWithMiddleware = applyMiddleware(thunkMiddleware, createLogger())(createStore)
+
+export default function configureStore(initialState){
+  const store = createStoreWithMiddleware(rootReducer, initialState)
+  return store
+}
