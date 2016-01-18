078abeeca7f859f669fc7c2cbacf127dec3222f2
Travis Johnston <tjohnst1@gmail.com>
2016-01-18 11:48:46 -0800

    create constants file

--- app/src/js/constants/constants.js
+++ app/src/js/constants/constants.js
@@ -1,3 +1,2 @@
-export default {
-  
-}
+export const REQUEST_LISTINGS = 'REQUEST_LISTINGS'
+export const RECEIVE_LISTINGS = 'RECEIVE_LISTINGS'
--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { configureStore } from '../stores/configureStore'
-import Home from './components/Home'
+// import { configureStore } from '../stores/configureStore'
+import App from './container/App'
 
-render(<Home />, document.getElementById('app'));
+// const store = configureStore()
+
+render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
