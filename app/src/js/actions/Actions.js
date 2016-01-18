f595203ce50593255567607a29f1c86259faeb89
Travis Johnston <tjohnst1@gmail.com>
2016-01-18 14:49:27 -0800

    update actions to include a fetchlistings function which makes an api call

--- app/src/js/actions/Actions.js
+++ app/src/js/actions/Actions.js
@@ -9,3 +9,12 @@ export function recieveListings(json){
   type: RECEIVE_LISTINGS,
   listings: json
 }
+
+export function fetchListings(){
+  return (dispatch) => {
+    dispatch(requestListings())
+    return fetch('http://api.bandsintown.com/artists/Common/events/recommended?location=PORTLAND&radius=150&app_id=anid&api_version=2.0&format=json')
+      .then((response) => response.json())
+      .then((json) => dispatch(recieveListings(json)))
+  }
+}
