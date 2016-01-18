a823c616831abe1572e89686c81351477e66a645
Travis Johnston <tjohnst1@gmail.com>
2016-01-18 15:52:23 -0800

    remove node event dispatcher

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -3,14 +3,9 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger'
-import { createStore } from '../stores/configureStore'
+import configureStore from '../stores/configureStore'
 import App from './container/App'
-import rootReducer from './reducers/reducers'
 
-const loggerMiddleware = createLogger()
-
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware,loggerMiddleware)(createStore)
-
-const store = createStoreWithMiddleware(rootReducer)
+const store = configureStore()
 
 render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
