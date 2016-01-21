5fc450722077fd4908de8582d2bce1d27230698f
Travis Johnston <tjohnst1@gmail.com>
2016-01-20 21:00:26 -0800

    refactor data in IndividualListings component into consts

--- app/src/js/components/IndividualListing.js
+++ app/src/js/components/IndividualListing.js
@@ -4,11 +4,15 @@ class IndividualListing extends Component {
 
   render(){
     const {event} = this.props
+    const date = event.start.date
+    const time = event.start.time
+    const artists = event.performance[0].artist.displayName
+    const venue = event.venue.displayName
     return (
       <div className="row">
-        <div className="col-sm-2">{event.start.date} @ {event.start.time}</div>
-        <div className="col-sm-5">{event.performance[0].artist.displayName}</div>
-        <div className="col-sm-5">{event.venue.displayName}</div>
+        <div className="col-sm-2">{date} @ {time}</div>
+        <div className="col-sm-5">{artists}</div>
+        <div className="col-sm-5">{venue}</div>
       </div>
     )
   }
