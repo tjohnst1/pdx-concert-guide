ca196a8f4abe0dac21fc49a5d534d2ac4d38be9a
Travis Johnston <tjohnst1@gmail.com>
2016-01-23 21:23:18 -0800

    update date format of individual postings with moment

--- app/src/js/components/IndividualListing.js
+++ app/src/js/components/IndividualListing.js
@@ -1,16 +1,17 @@
 import React, { Component, PropTypes } from 'react'
 import {capitalize} from '../util/utilities'
+import moment from 'moment'
 
 class IndividualListing extends Component {
   render(){
     const {event} = this.props
-    const date = event.start.date
-    const time = event.start.time
+    const date = moment(event.start.date).format("ddd Do")
+    // const time = moment(event.start.datetime).format("hh:mm a")
     const artists = event.performance.map((artist) => capitalize(artist.displayName)).join(", ")
     const venue = event.venue.displayName
     return (
       <div className="row">
-        <div className="col-sm-2">{date} @ {time}</div>
+        <div className="col-sm-2">{date}</div>
         <div className="col-sm-5">{artists}</div>
         <div className="col-sm-5">{venue}</div>
       </div>
